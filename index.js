91d8f6a621ccb5e601adac3d901e8540096c25da
alamnahid <nahidalam13765@gmail.com>
2023-12-20 15:21:13 +0600

    create apis for cart collections

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const mobilecollection = client.db("mobileshopdb").collection("mobiles")
+    const cartCollection = client.db("mobileshopdb").collection("carts")
 
     // jwt related apis
 
@@ -95,6 +96,34 @@ async function run() {
         res.status(500).json({ error: 'Internal Server Error' });
       }
     });
+
+    app.get('/allmobile/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await mobilecollection.findOne(query);
+      res.send(result);
+    })
+
+    //cart collections
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result);
+    })
 
     // stats
     app.get('/mobile-stats', async (req, res) => {
